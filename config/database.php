9026f368eb2fe9bc98f3520093920931afdba657
Joe Huss <detain@interserver.net>
2021-03-30 01:35:12 -0400

    updates adding db setup

--- config/database.php
+++ config/database.php
@@ -14,53 +14,68 @@
 
 return [
 
-    'default' => 'mysql',
+	'default' => 'mysql',
 
-    'connections' => [
+	'connections' => [
 
-        'mysql' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ],
+		'mysql' => [
+			'driver' => 'mysql',
+			'host' => env('DB_HOST', '127.0.0.1'),
+			'port' => env('DB_PORT', '3306'),
+			'database' => env('DB_DATABASE', 'forge'),
+			'username' => env('DB_USERNAME', 'forge'),
+			'password' => env('DB_PASSWORD', ''),
+			'unix_socket' => env('DB_SOCKET', ''),
+			'charset' => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix' => '',
+			'strict' => true,
+			'engine' => null,
+		],
 
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', ''),
-            'prefix' => '',
-        ],
+		'zonemta' => [
+			'driver' => 'mysql',
+			'host' => env('ZONEMTA_HOST', '127.0.0.1'),
+			'port' => env('ZONEMTA_PORT', '3306'),
+			'database' => env('ZONEMTA_DB', 'forge'),
+			'username' => env('ZONEMTA_USERNAME', 'forge'),
+			'password' => env('ZONEMTA_PASSWORD', ''),
+			'unix_socket' => env('ZONEMTA_SOCKET', ''),
+			'charset' => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix' => '',
+			'strict' => true,
+			'engine' => null,
+		],
 
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'prefer',
-        ],
+		'sqlite' => [
+			'driver' => 'sqlite',
+			'database' => env('DB_DATABASE', ''),
+			'prefix' => '',
+		],
 
-        'sqlsrv' => [
-            'driver' => 'sqlsrv',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-        ],
-    ],
+		'pgsql' => [
+			'driver' => 'pgsql',
+			'host' => env('DB_HOST', '127.0.0.1'),
+			'port' => env('DB_PORT', '5432'),
+			'database' => env('DB_DATABASE', 'forge'),
+			'username' => env('DB_USERNAME', 'forge'),
+			'password' => env('DB_PASSWORD', ''),
+			'charset' => 'utf8',
+			'prefix' => '',
+			'schema' => 'public',
+			'sslmode' => 'prefer',
+		],
+
+		'sqlsrv' => [
+			'driver' => 'sqlsrv',
+			'host' => env('DB_HOST', 'localhost'),
+			'port' => env('DB_PORT', '1433'),
+			'database' => env('DB_DATABASE', 'forge'),
+			'username' => env('DB_USERNAME', 'forge'),
+			'password' => env('DB_PASSWORD', ''),
+			'charset' => 'utf8',
+			'prefix' => '',
+		],
+	],
 ];
