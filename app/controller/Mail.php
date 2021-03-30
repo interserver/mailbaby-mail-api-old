43a833405d3e7715461da8b107b9eb1511d69aa1
Joe Huss <detain@interserver.net>
2021-03-30 03:26:10 -0400

    updates

--- app/controller/Mail.php
+++ app/controller/Mail.php
@@ -2,11 +2,32 @@
 namespace app\controller;
 
 use support\Request;
+use support\Db;
 
 class Mail
 {
 	public function index(Request $request)
 	{
+		$login = $request->header('x-api-login');
+		$pass = $request->header('x-api-pass');
+		$key = $request->header('x-api-key');
+		if (is_null($login) || (is_null($pass) || is_null($key))) {
+			return response('API key is missing or invalid', 401);
+		}
+		if (!is_null($pass)) {
+			$name = Db::table('accounts')
+				->where('account_lid', $login)
+				->where('account_passwd', md5($pass))
+				->get();
+		}
+		if (!is_null($key)) {
+			$name = Db::table('accounts')
+				->leftJoin('account_security', 'account_security.account_id','=','accounts.account_id')
+				->where('account_lid', $login)
+				->where('account_sec_type', 'api_key')
+				->where('account_sec_data', $key)
+				->get();
+		}
 		return response('hello webman');
 	}
 
---
+++ app/middleware/AuthCheck.php
@@ -0,0 +1,18 @@
+<?php
+namespace app\middleware;
+
+use Webman\MiddlewareInterface;
+use Webman\Http\Response;
+use Webman\Http\Request;
+
+class AuthCheck implements MiddlewareInterface
+{
+    public function process(Request $request, callable $next) : Response
+    {
+        $session = $request->session();
+        if (!$session->get('userinfo')) {
+            return redirect('/user/login');
+        }
+        return $next($request);
+    }
+}
