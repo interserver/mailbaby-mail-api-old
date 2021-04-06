1e597d8af38b9c21a43750d9e8ba625f9b50f817
Joe Huss <detain@interserver.net>
2021-04-06 14:18:08 -0400

    updates adding samples

---
+++ samples/jaxrs-jersey/src/main/java/io/swagger/api/factories/PingApiServiceFactory.java
@@ -0,0 +1,12 @@
+package io.swagger.api.factories;
+
+import io.swagger.api.PingApiService;
+import io.swagger.api.impl.PingApiServiceImpl;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaJerseyServerCodegen", date = "2021-04-06T14:14:57.434748-04:00[America/New_York]")public class PingApiServiceFactory {
+    private final static PingApiService service = new PingApiServiceImpl();
+
+    public static PingApiService getPingApi() {
+        return service;
+    }
+}
