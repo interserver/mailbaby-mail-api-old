cc3dcdc2c214232124bcbe269a3efbd5d7d8807b
Joe Huss <detain@interserver.net>
2021-11-16 16:01:11 -0500

    cors update

--- app/middleware/AccessControl.php
+++ app/middleware/AccessControl.php
@@ -12,12 +12,12 @@ class AccessControl implements MiddlewareInterface
 		//$response = $request->method() == 'OPTIONS' ? response('') : $next($request);
 		$response = $next($request);
 		$response->withHeaders([
-			'Accept' => '*',
-			'Access-Control-Allow' => '*',
+			//'Accept' => '*',
+			//'Access-Control-Allow' => '*',
 			'Access-Control-Allow-Origin' => '*',
-			'Access-Control-Expose-Headers' => 'Content-Length,X-JSON',
+			//'Access-Control-Expose-Headers' => 'Content-Length,X-JSON',
 			'Access-Control-Allow-Methods' => 'GET,POST,PATCH,PUT,DELETE,OPTIONS',
-			'Access-Control-Allow-Headers' => 'Content-Type,Authorization,Accept,Origin,Accept-Language,X-Authorization,X-Requested-With'
+			'Access-Control-Allow-Headers' => 'Content-Type,Authorization,Accept,Origin,Accept-Language,X-Authorization,X-API-KEY,X-Requested-With'
 		]);
 
 		return $response;
